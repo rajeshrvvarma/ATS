6deab82430ffb75c82b95d9a39d288cca90835dd
mskumargvd <31104360+mskumargvd@users.noreply.github.com>
2025-09-29 17:18:31 +0530

    fs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ATStatic/', // <-- IMPORTANT: Change this to your GitHub repository name!
+  base: '/', // <-- IMPORTANT: Change this to your GitHub repository name!
 })
