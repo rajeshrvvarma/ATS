9fd85c71c4db6503a73f656416cf474fbc0e7e7c
rajeshrvvarma <rajesh.rv.varma@gmail.com>
2025-10-11 22:53:46 +0530

    8989789

--- src/components/UPIQRCode.jsx
+++ src/components/UPIQRCode.jsx
@@ -1,12 +1,12 @@
 // Simple React QR code component using 'qrcode.react' (to be installed)
 import React from 'react';
-import QRCode from 'qrcode.react';
+import QRCode from 'react-qr-code';
 
 export default function UPIQRCode({ upiUrl, size = 160 }) {
   if (!upiUrl) return null;
   return (
     <div className="flex flex-col items-center gap-2 mt-2">
-      <QRCode value={upiUrl} size={size} level="H" includeMargin={true} />
+      <QRCode value={upiUrl} size={size} />
       <span className="text-xs text-slate-400">Scan to pay via UPI</span>
     </div>
   );
